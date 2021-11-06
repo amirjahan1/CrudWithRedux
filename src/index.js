5ac0b3cba9089d9fc07fb595a1f83de0b97cb1a2
Amirjahan_1 <amirjahan454@gmail.com>
2021-11-06 13:17:21 +0330

    Add react Router Dom

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from "react-redux";
 import store from "./Redux/Store";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+      <BrowserRouter>
+          <Provider store={store}>
+              <App />
+          </Provider>
 
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
